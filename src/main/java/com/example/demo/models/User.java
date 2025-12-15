03f69a04f794df0853e49761f25fc5bdca3f2182
bala-amypo <bala@amypo.in>
2025-12-15 13:55:04 +0000

    User commit

---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,19 @@
+package com.example.demo.models;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    public User(){}
+    public User(String name, String email, String password, String role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+    
+
+    
+}
